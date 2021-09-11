d344735405c918a641454a156721e18e2f676184
TonnieGathogo <gathogoanthony2@gmail.com>
2021-09-11 22:48:19 +0300

    test

--- routes/catalog.js
+++ routes/catalog.js
@@ -2,14 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 // Require controller modules.
-var book_controller = require('../controllers/bookController');
 var author_controller = require('../controllers/authorController');
+var book_controller = require('../controllers/bookController');
 var genre_controller = require('../controllers/genreController');
 var book_instance_controller = require('../controllers/bookinstanceController');
 
-router.get('/', function (req, res) {
-    res.redirect('/catalog')
-});
+
 
 /// BOOK ROUTES ///
 
